5c77db2317677ebe9b1f993287dab1856405784f
Carlo10K <carlosortegadiaz50@gmail.com>
2022-12-26 10:11:17 -0600

    [FIX]Automatic Test with junit annotattions

--- src/test/java/org/example/util/StringUtilTest.java
+++ src/test/java/org/example/util/StringUtilTest.java
@@ -1,10 +1,14 @@
 package org.example.util;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class StringUtilTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void TestRepeat(){
         Assertions.assertEquals("holaholahola", StringUtil.repeat("hola",3));
         Assertions.assertEquals("hola", StringUtil.repeat("hola",1));
     }
 }
+
