709be150b2615af187487b7389c3673621ce3d7c
Carlo10K <carlosortegadiaz50@gmail.com>
2022-12-26 09:55:02 -0600

    Automatic Test

--- src/test/java/org/example/util/StringUtilTest.java
+++ src/test/java/org/example/util/StringUtilTest.java
@@ -2,17 +2,9 @@ package org.example.util;
 
 import org.junit.jupiter.api.Assertions;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class StringUtilTest {
     public static void main(String[] args) {
-        assertEquals(StringUtil.repeat("Hola", 3), "HolaHolaHola");
-        assertEquals(StringUtil.repeat("Hola", 1), "Hola");
-    }
-
-    private static void assertEquals(String actual, String expected) {
-        if (!actual.equals(expected)) {
-            throw new RuntimeException(actual + " is not equals to " + expected);
-        }
+        Assertions.assertEquals("holaholahola", StringUtil.repeat("hola",3));
+        Assertions.assertEquals("hola", StringUtil.repeat("hola",1));
     }
 }
