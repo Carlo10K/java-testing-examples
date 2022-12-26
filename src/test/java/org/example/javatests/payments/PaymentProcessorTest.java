a450ba6cb22df25004789dc2e3e71da0633749b2
Carlo10K <carlosortegadiaz50@gmail.com>
2022-12-26 12:03:10 -0600

    Payments

--- src/test/java/org/example/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/org/example/javatests/payments/PaymentProcessorTest.java
@@ -1,27 +1,36 @@
 package org.example.javatests.payments;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentProcessorTest {
+
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @BeforeEach
+    public void setup(){
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     void paymentIsCorrect() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
         Assertions.assertTrue(paymentProcessor.makePayment(1000));
     }
 
     @Test
     void paymentIsWrong() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
         Assertions.assertFalse(paymentProcessor.makePayment(1000));
     }
 }
